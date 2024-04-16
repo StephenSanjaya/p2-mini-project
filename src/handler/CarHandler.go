1763e61c0961402bff96617df0cc6c3be0f74946
StephenSanjaya <stephensanjaya5@gmail.com>
2024-04-17 00:42:26 +0700

    refactor: move some func to helper folder

--- src/handler/CarHandler.go
+++ src/handler/CarHandler.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"p2-mini-project/src/dto"
 	"p2-mini-project/src/entity"
+	"p2-mini-project/src/helpers"
 	"p2-mini-project/src/httputil"
 	"time"
 
@@ -67,7 +68,7 @@ func (cs *CarService) RentalCar(c *gin.Context) {
 		c.Error(httputil.NewError(http.StatusBadRequest, "RentalCar: invalid body request", err))
 		return
 	}
-	price, err := GetPrice(cs, c, rental)
+	price, err := helpers.GetPrice(cs.db, rental)
 	if err != nil {
 		c.Error(err)
 		return
@@ -110,29 +111,38 @@ func (cs *CarService) PayRentalCar(c *gin.Context) {
 	}
 
 	payment.PaymentDate = time.Now().Format("2006-01-02")
-	payment.TotalPrice = CalculateTotalPrice(payment, rental)
+	// payment.TotalPrice = CalculateTotalPrice(payment, rental)
+	payment.TotalPrice = helpers.CalculateTotalPrice(payment, rental)
 	payment.RentalID = rental.ID
+	payment.PaymentStatus = "settlement"
+
+	// check rental cost
+	// currDeposit, err := GetUserDeposit(cs, int(c.GetFloat64("user_id")))
+	currDeposit, err := helpers.GetUserDeposit(cs.db, int(c.GetFloat64("user_id")))
+	if err != nil {
+		c.Error(err)
+		return
+	}
+	if currDeposit < payment.TotalPrice {
+		paymentError := fmt.Sprintf("your deposit is %.2f while total payment is %.2f", currDeposit, payment.TotalPrice)
+		c.Error(httputil.NewError(http.StatusBadRequest, "PayRentalCar: your deposit is not enough", errors.New(paymentError)))
+		return
+	}
+	updatedDeposit := currDeposit - payment.TotalPrice
 
 	txErr := cs.db.Transaction(func(tx *gorm.DB) error {
-		// check rental cost & update deposit
-		currDeposit, err := GetUserDeposit(tx, int(c.GetFloat64("user_id")))
-		if err != nil {
-			return err
-		}
-		if currDeposit < payment.TotalPrice {
-			paymentError := fmt.Sprintf("your deposit is %.2f while total payment is %.2f", currDeposit, payment.TotalPrice)
-			return httputil.NewError(http.StatusBadRequest, "PayRentalCar: your deposit is not enough", errors.New(paymentError))
-		}
-		updatedDeposit := currDeposit - payment.TotalPrice
 
+		// update deposit
 		if res := tx.Model(&entity.User{}).Where("user_id = ?", int(c.GetFloat64("user_id"))).Update("deposit", updatedDeposit); res.Error != nil {
 			return httputil.NewError(http.StatusInternalServerError, "PayRentalCar: failed to update deposit", res.Error)
 		}
 
+		// create payment
 		if res := tx.Create(&payment); res.Error != nil {
 			return httputil.NewError(http.StatusInternalServerError, "PayRentalCar: failed to pay rental car", res.Error)
 		}
 
+		// update status
 		if res := tx.Model(&entity.Car{}).Where("car_id = ?", rental.CarID).Update("status", "rented"); res.Error != nil {
 			return httputil.NewError(http.StatusInternalServerError, "PayRentalCar: failed to update status", res.Error)
 		}
@@ -176,45 +186,22 @@ func (cs *CarService) ReturnRentalCar(c *gin.Context) {
 	})
 }
 
-func GetUserDeposit(tx *gorm.DB, user_id int) (float64, *httputil.HTTPError) {
-	deposit := 0.0
-	if res := tx.Table("users").Select("deposit").Where("user_id = ?", user_id).Scan(&deposit); res.Error != nil {
-		return -1, httputil.NewError(http.StatusInternalServerError, "GetUserDeposit: fail to get deposit user", res.Error)
-	}
-	return deposit, nil
-}
+// func GetUserDeposit(cs *CarService, user_id int) (float64, *httputil.HTTPError) {
+// 	deposit := 0.0
+// 	if res := cs.db.Table("users").Select("deposit").Where("user_id = ?", user_id).Scan(&deposit); res.Error != nil {
+// 		return -1, httputil.NewError(http.StatusInternalServerError, "GetUserDeposit: fail to get deposit user", res.Error)
+// 	}
+// 	return deposit, nil
+// }
 
-func GetPrice(cs *CarService, c *gin.Context, r *dto.Rental) (float64, *httputil.HTTPError) {
-	price := 0.0
-	res := cs.db.Model(&entity.Car{}).Select("rental_cost_per_day").Where("car_id = ?", r.CarID).First(&price)
-	if errors.Is(res.Error, gorm.ErrRecordNotFound) {
-		return -1, httputil.NewError(http.StatusNotFound, "GetPrice: car id not found", res.Error)
-	}
-	if res.Error != nil {
-		return -1, httputil.NewError(http.StatusInternalServerError, "GetPrice: fail to check stock", res.Error)
-	}
-	return price, nil
-}
-
-func CalculateTotalPrice(p *dto.Payment, r *dto.Rental) float64 {
-
-	returnDate, _ := time.Parse(time.RFC3339, r.ReturnDate)
-	rentalDate, _ := time.Parse(time.RFC3339, r.RentalDate)
-
-	diffDay := int(returnDate.Sub(rentalDate).Hours() / 24)
-
-	total_price := (r.Price * float64(diffDay))
-
-	switch p.CouponID {
-	case 1:
-		total_price = total_price - (total_price * 0.1)
-	case 2:
-		total_price = total_price - (total_price * 0.2)
-	case 3:
-		total_price = total_price - (total_price * 0.3)
-	default:
-		fmt.Println("can't detect coupon")
-	}
-
-	return total_price
-}
+// func GetPrice(cs *CarService, r *dto.Rental) (float64, *httputil.HTTPError) {
+// 	price := 0.0
+// 	res := cs.db.Model(&entity.Car{}).Select("rental_cost_per_day").Where("car_id = ?", r.CarID).First(&price)
+// 	if errors.Is(res.Error, gorm.ErrRecordNotFound) {
+// 		return -1, httputil.NewError(http.StatusNotFound, "GetPrice: car id not found", res.Error)
+// 	}
+// 	if res.Error != nil {
+// 		return -1, httputil.NewError(http.StatusInternalServerError, "GetPrice: fail to check stock", res.Error)
+// 	}
+// 	return price, nil
+// }
