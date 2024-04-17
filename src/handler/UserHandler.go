59bbb72d009521e1e7ebdd0c7dc6488eecc592b2
StephenSanjaya <stephensanjaya5@gmail.com>
2024-04-17 19:33:14 +0700

    feat: add top up func

---
+++ src/handler/UserHandler.go
@@ -0,0 +1,47 @@
+package handler
+
+import (
+	"net/http"
+	"p2-mini-project/src/dto"
+	"p2-mini-project/src/entity"
+	"p2-mini-project/src/helpers"
+	"p2-mini-project/src/httputil"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type UserService struct {
+	db *gorm.DB
+}
+
+func NewUserService(db *gorm.DB) *UserService {
+	return &UserService{db: db}
+}
+
+func (us *UserService) TopUp(c *gin.Context) {
+
+	topup := new(dto.TopUp)
+
+	if err := c.ShouldBindJSON(&topup); err != nil {
+		c.Error(httputil.NewError(http.StatusBadRequest, "TopUp: invalid body request", err))
+		return
+	}
+
+	user_id := int(c.GetFloat64("user_id"))
+
+	user, err := helpers.GetCurrentUser(us.db, user_id)
+	if err != nil {
+		c.Error(err)
+	}
+
+	updatedDeposit := user.Deposit + topup.Amount
+
+	if res := us.db.Model(&entity.User{}).Where("user_id = ?", user_id).Update("deposit", updatedDeposit); res.Error != nil {
+		c.Error(httputil.NewError(http.StatusInternalServerError, "TopUp: failed to top up", res.Error))
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "success top up",
+	})
+}
